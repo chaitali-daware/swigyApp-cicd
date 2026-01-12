3b937a20c7b62cbe358edca904a013816af4cf61
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2026-01-12 08:59:48 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -40,4 +40,4 @@ def static_files(path):
     return send_from_directory(FRONTEND_DIR, path)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)

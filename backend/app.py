6e01ece2ad128e32de17f8baf1599581e214e5f4
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2026-01-15 12:30:32 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import os
 
-# Get absolute paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FRONTEND_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "frontend"))
+FRONTEND_DIR = os.path.join(BASE_DIR, "..", "frontend")
+STATIC_DIR = FRONTEND_DIR   # frontend itself contains static files
 
 app = Flask(
     __name__,
-    static_folder=FRONTEND_DIR,
+    static_folder=STATIC_DIR,
+    static_url_path="/static",
     template_folder=FRONTEND_DIR
 )
 
@@ -20,18 +21,17 @@ menu = [
     {"id": 3, "name": "Biryani", "price": 249, "image": "images/Biryani.jpg"}
 ]
 
-@app.route("/menu")
-def get_menu():
-    return jsonify(menu)
-
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/menu")
+def get_menu():
+    return jsonify(menu)
 
 @app.route("/order/<item>", methods=["POST"])
 def order_food(item):
     return jsonify({"message": f"{item} ordered successfully!"})
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)

255ed6603e50d8dbe68dc97a6c96d88dcb07f760
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2026-01-15 11:17:16 +0530

    script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,25 @@
-fetch("http://localhost:5000/menu")
+fetch("/menu")
     .then(response => response.json())
     .then(data => {
         const foodDiv = document.getElementById("food");
+        foodDiv.innerHTML = "";
 
         data.forEach(item => {
             foodDiv.innerHTML += `
                 <div class="card">
-                    <img src="${item.image}" alt="${item.name}">
+                    <img src="/static/${item.image}" alt="${item.name}">
                     <h3>${item.name}</h3>
                     <p>₹${item.price}</p>
                     <button onclick="orderFood('${item.name}')">Order Now</button>
                 </div>
             `;
         });
-    });
+    })
+    .catch(err => console.error("Menu load error:", err));
 
 function orderFood(item) {
-    fetch(`http://localhost:5000/order/${item}`, {
-        method: "POST"
-    })
-    .then(res => res.json())
-    .then(data => alert(data.message));
+    fetch(`/order/${item}`, { method: "POST" })
+        .then(res => res.json())
+        .then(data => alert(data.message))
+        .catch(err => console.error("Order error:", err));
 }
